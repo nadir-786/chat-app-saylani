2af74eb7aaddf2157657a94069cb98c5fcab36c8
nadirdeveloper <63139854+nadirdeveloper@users.noreply.github.com>
2020-09-21 15:18:47 +0500

    first commit

---
+++ public/javascript/auth.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAhNdmy_g5TNqfJQDcedGtCNKrooG3-aMs",
+    authDomain: "chat-app-saylani.firebaseapp.com",
+    databaseURL: "https://chat-app-saylani.firebaseio.com",
+    projectId: "chat-app-saylani",
+    storageBucket: "chat-app-saylani.appspot.com",
+    messagingSenderId: "496676840031",
+    appId: "1:496676840031:web:238240b838ce0032dbf1ea"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
